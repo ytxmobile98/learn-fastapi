4eb596aa1f4c2e033b9b0dfdf2dde2ce574bc67b
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-07-29 11:03:45 +0800

    feat: added `PUT /items/{item_id}`

--- intro/main.py
+++ intro/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
 
 
 @app.get("/")
@@ -12,3 +19,8 @@ async def read_root() -> dict[str, str]:
 async def read_item(item_id: str, q: str | None = None) \
         -> dict[str, str | None]:
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item) -> dict[str, int | Item]:
+    return {"item_id": item_id, "item": item}
