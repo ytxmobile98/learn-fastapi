72a606fce85e1ab0eefe434f6a9079c15c878032
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-07-29 10:51:31 +0800

    style: added types

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
+async def read_root() -> dict[str, str]:
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: str, q: str | None = None) \
+        -> dict[str, str | None]:
     return {"item_id": item_id, "q": q}
